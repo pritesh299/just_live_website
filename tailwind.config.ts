0aa0a186807bceb0210086cd2947ea36ec0d9e0b
Pritesh Jogdhanakar <129420569+pritesh299@users.noreply.github.com>
2024-07-04 19:06:02 +0530

    Created next.js app

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Tenor_Sans: ["Tenor Sans", 'sans-serif'],
+       },
+    },
+  },
+  plugins: [],
+};
+export default config;
